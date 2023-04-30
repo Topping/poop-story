657449dca6f17d053663554ac17d727763d04b7e
Martin <marwynskyggfisk@gmail.com>
2023-04-30 20:02:48 +0000

    add best frontend

---
+++ frontend/poop-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { AuthModule } from "@auth0/auth0-angular";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: "poopstory.eu.auth0.com",
+      clientId: "4UDKesdZeGk4oZpbNZ6mA5jxi8wcjNwg",
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
