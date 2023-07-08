cfaf81a49afea5d17c978f66c490b42f66e4b478
niktop <nikolai.topping@gmail.com>
2023-07-08 17:02:11 +0200

    Better exception handling in backend

--- backend/src/main/java/poop/story/backend/application/exception/PersistFailedException.java
+++ backend/src/main/java/poop/story/backend/application/exception/PersistFailedException.java
@@ -1,7 +1,21 @@
 package poop.story.backend.application.exception;
 
 public class PersistFailedException extends RuntimeException {
-    public PersistFailedException(String message) {
+    private final boolean userError;
+    public PersistFailedException(String message, boolean userError) {
         super(message);
+        this.userError = true;
+    }
+
+    public static PersistFailedException userError(String message) {
+        return new PersistFailedException(message, true);
+    }
+
+    public static PersistFailedException serverError(String message) {
+        return new PersistFailedException(message, false);
+    }
+
+    public boolean userError() {
+        return userError;
     }
 }
